434e15435a56a0e99b40e7a76c548f9a0251bea7
danielnguyen446 <77251383+danielnguyen446@users.noreply.github.com>
2021-01-19 18:09:30 -0800

    Update CovidGenomeAnalysis.java

--- CovidGenomeAnalysis.java
+++ CovidGenomeAnalysis.java
@@ -17,34 +17,31 @@ public class CovidGenomeAnalysis
 {
 	public static void main(String[] args)
 	{
-
-	    String seq; //store the genome sequence
-	    String newSeq;  //the complementary sequence
-	    char base;
-	    int thymineCount=0; //how many letter T's there are in the output
-	    
+		String seq; //store the genome sequence
+		String newSeq;  //the complementary sequence
+		char base;
+		int thymineCount=0; //how many letter T's there are in the output
 	    
 		Scanner scnr = new Scanner(System.in);  //scan the sequence into seq
-        seq = scnr.next();
-        
-        
-        for(int a=0; a<seq.length(); a++)   //loop. do this for each letter in the sequence
-        {
-            base = seq.charAt(a);   //the base to examine
-            if (base =='A')
-            {
-                thymineCount++; //increment thymine count.
-            }
-        }   
+        	seq = scnr.next();
+		
+        	for(int a=0; a<seq.length(); a++)   //loop. do this for each letter in the sequence
+        	{
+        		base = seq.charAt(a);   //the base to examine
+        		if (base =='A')
+        		{
+                		thymineCount++; //increment thymine count.
+        		}
+		}   
             
-        seq = seq.replace('A', 'B');    //temporarily move A to B, since the code can't perform the replacements simultaneously
-        seq = seq.replace('T', 'A');
-        seq = seq.replace('B', 'T');    //can only make it T after T has been replaced.
+        	seq = seq.replace('A', 'B');    //temporarily move A to B, since the code can't perform the replacements simultaneously
+        	seq = seq.replace('T', 'A');
+        	seq = seq.replace('B', 'T');    //can only make it T after T has been replaced.
         
-        seq = seq.replace('C', 'D');    //same for C. temporary replacement.
-        seq = seq.replace('G', 'C'); 
-        seq = seq.replace('D', 'G'); 
+        	seq = seq.replace('C', 'D');    //same for C. temporary replacement.
+        	seq = seq.replace('G', 'C'); 
+        	seq = seq.replace('D', 'G'); 
     
-        System.out.println(thymineCount + " " + seq); //print the output.
+        	System.out.println(thymineCount + " " + seq); //print the output.
 	}
 }
