e79992cf65934046ac5f3b07cd5f115a60976246
danielnguyen446 <77251383+danielnguyen446@users.noreply.github.com>
2021-01-19 22:03:30 -0800

    Update CovidMutation.java

--- CovidMutation.java
+++ CovidMutation.java
@@ -10,44 +10,43 @@
 import java.util.Scanner;
 
 /**
- * This class will prompt the user to enter the string of characters as well as an integer on the next line. 
- * The integer k will specify the chunk size.
- * The program will print each of the chunks in reverse with a nested loop. If there is a final chunk that is smaller than length k, the program will also print this in reverse.
+ * This class will prompt the user to enter the string of characters as well as an integer on the next line. It will then modify the string by reversing character chunks of length k.
+ * It will print the chunks in reverse. If there is a final chunk that is smaller than length k, the program will also print this in reverse.
  */
 
 public class CovidMutation
 {
 	public static void main(String[] args)
 	{
-		String seq; //store the genome sequence
+		String seq; /**store the genome sequence*/
 		String chunk;
-		String babyChunk;   //in case there is a remainder chunk
-		int k=0;    //chunk size
+		String babyChunk;   /**in case there is a remainder chunk*/
+		int k=0;    /**chunk size*/
 		
-		Scanner scnr = new Scanner(System.in);  //scan the sequence
+		Scanner scnr = new Scanner(System.in);  /**scan the sequence*/
 		seq = scnr.next();
 		k = scnr.nextInt();
 		
-    int numberChunks = seq.length()/k;  //find the number of chunks
+        	int numberChunks = seq.length()/k;  /**find the number of chunks*/
 		
 		for(int a=0; a<numberChunks; a++)
 		{
-            chunk = seq.substring(a*k, a*k+k);  //split the string into chunks
+            		chunk = seq.substring(a*k, a*k+k);  /**split the string into chunks*/
             
-            for(int b=k-1; b>=0; b--)   //print the chunk, but starting from the value of k index
-            {
-                System.out.print(chunk.charAt(b));
-            }
+           		for(int b=k-1; b>=0; b--)   /**print the chunk, but starting from the value of k index*/
+            		{
+                		System.out.print(chunk.charAt(b));
+            		}
 		}
 		
-		if(seq.length()%k!=0)   //in case of remainder chunk
-        {
-            babyChunk = seq.substring(numberChunks*k, seq.length());  //make a remainder chunk
+		if(seq.length()%k!=0)   /**in case of remainder chunk*/
+        	{
+            		babyChunk = seq.substring(numberChunks*k, seq.length());  /**make a remainder chunk*/
             
-            for(int c=babyChunk.length()-1; c>=0; c--)  //lastly, print the remainder babyChunk in reverse
-            {
-                System.out.print(babyChunk.charAt(c));
-            }
-        }
+            		for(int c=babyChunk.length()-1; c>=0; c--)  /**lastly, print the remainder babyChunk in reverse*/
+            		{
+                		System.out.print(babyChunk.charAt(c));
+            		}
+        	}
 	}
 }
