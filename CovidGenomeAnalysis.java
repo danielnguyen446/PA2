f80b9c1666cd2eaff07a864419cbd98f5a42a71b
danielnguyen446 <77251383+danielnguyen446@users.noreply.github.com>
2021-01-19 21:55:18 -0800

    Update CovidGenomeAnalysis.java

--- CovidGenomeAnalysis.java
+++ CovidGenomeAnalysis.java
@@ -17,30 +17,30 @@ public class CovidGenomeAnalysis
 {
 	public static void main(String[] args)
 	{
-		String seq; //store the genome sequence
+		String seq; /**store the genome sequence*/
 		char base;
-		int thymineCount=0; //how many letter T's there are in the output
+		int thymineCount=0; /**how many letter T's there are in the output*/
 	    
-		Scanner scnr = new Scanner(System.in);  //scan the sequence into seq
+		Scanner scnr = new Scanner(System.in);  /**scan the sequence into seq*/
         	seq = scnr.next();
 		
-        	for(int a=0; a<seq.length(); a++)   //loop. do this for each letter in the sequence
+        	for(int a=0; a<seq.length(); a++)   /**loop. do this for each letter in the sequence*/
         	{
-        		base = seq.charAt(a);   //the base to examine
+        		base = seq.charAt(a);   /**the base to examine*/
         		if (base =='A')
         		{
-                		thymineCount++; //increment thymine count.
+                		thymineCount++; /**increment thymine count.*/
         		}
 		}   
             
-        	seq = seq.replace('A', 'B');    //temporarily move A to B, since the code can't perform the replacements simultaneously
+        	seq = seq.replace('A', 'B');    /**temporarily move A to B, since the code can't perform the replacements simultaneously*/
         	seq = seq.replace('T', 'A');
-        	seq = seq.replace('B', 'T');    //can only make it T after T has been replaced.
+        	seq = seq.replace('B', 'T');    /**can only make it T after T has been replaced.*/
         
-        	seq = seq.replace('C', 'D');    //same for C. temporary replacement.
+        	seq = seq.replace('C', 'D');    /**same for C. temporary replacement.*/
         	seq = seq.replace('G', 'C'); 
         	seq = seq.replace('D', 'G'); 
     
-        	System.out.println(thymineCount + " " + seq); //print the output.
+        	System.out.println(thymineCount + " " + seq); /**print the output.*/
 	}
 }
